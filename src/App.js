b27a156216b7a03ef12f3575ecb4c4602f76fd17
Har1s-Akbar <stanker801@gmail.com>
2023-05-28 14:11:58 -0700

    frontend for django api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+
 
 function App() {
+  const [Data, setData] = useState([])
+  const url = 'http://localhost:8000/api/products'
+  async function fetchData () {
+    try{
+      const response = await fetch(url);
+      if(!response.ok){
+        throw new Error(`HTTP error: ${response.status}`);
+      }
+      const data = await response.json();
+      return (setData(data))
+    }catch(error){
+      console.error('errorr')
+    }
+  }
+
+  useEffect(()=>{
+    fetchData() 
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {Data.map(item =>(
+        <div key={item.id}>
+          <h1>{item.title}</h1>
+          <div>
+            <p>{item.desc}</p>
+            <p>{item.added}</p>
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
